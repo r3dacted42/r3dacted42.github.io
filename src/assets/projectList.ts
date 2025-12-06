d72d70c18d4a8841455fea49982e4ef4a578e559
priyansh agrahari <75441026+r3dacted42@users.noreply.github.com>
2025-12-06 12:43:17 +0530

    Add new projects to projectList

--- src/assets/projectList.ts
+++ src/assets/projectList.ts
@@ -3,8 +3,10 @@ import type { WindowStyle } from "../types";
 
 export default [
     { repo: "sparrow-vps", windowStyle: { fgColor: colors.black.ff, bgColor: colors.yellow.a8 } },
+    { repo: "static-null-analyzer", windowStyle: { fgColor: colors.purple.ff, bgColor: colors.yellow.a8 } },
     { repo: "stego", windowStyle: { fgColor: colors.black.a8, bgColor: colors.white.ff } },
     { repo: "maxwell", title: "maxwell appreciation page", windowStyle: { bgColor: colors.orange.a8 } },
+    { repo: "cursor-inf-util", windowStyle: { bgColor: colors.cyan.a8 } },
     { repo: "tic-tac-toe", windowStyle: { bgColor: colors.black.a8 } },
     { repo: "turtle-graphics", title: "webgl turtle graphics", windowStyle: { bgColor: colors.green.a8 } },
     { repo: "bank-mgmt-sys", title: "ncurses bank mgmt sys" },
