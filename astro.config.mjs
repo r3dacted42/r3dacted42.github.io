a5c0f7b2583ef3fb3da12209f2695cdbfa1bebca
priyansh agrahari <75441026+r3dacted42@users.noreply.github.com>
2025-12-16 15:57:22 +0530

    use fontless for font integration
    change heading font

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import vue from '@astrojs/vue';
 import mdx from '@astrojs/mdx';
+import { fontless } from 'fontless';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://r3dacted42.github.io",
-  integrations: [vue(), mdx()]
+  vite: { plugins: [fontless()] },
+  integrations: [vue(), mdx()],
 });
